b0bcb124646da41aa9bb81447fb7c096abe0c072
Bruno Azambuja Carvalho <brunoazca@gmail.com>
2025-02-03 16:08:12 -0300

    Project created

---
+++ UberEats/UberEats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UberEats
+//
+//  Created by Bruno Azambuja Carvalho on 03/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UberEats/UberEats/UberEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberEatsApp.swift
+//  UberEats
+//
+//  Created by Bruno Azambuja Carvalho on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct UberEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
